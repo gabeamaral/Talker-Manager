1d7549b27da3408d8be3039904618d177cf4b0b3
Gabriel Duarte <gabrielduarte1405@gmail.com>
2023-03-14 18:34:35 -0300

    Requisito 1

--- src/index.js
+++ src/index.js
@@ -1,14 +1,21 @@
 const express = require('express');
+const fs = require('fs');
 
 const app = express();
 app.use(express.json());
-//oioi
+
+const json = 'src/talker.json';
 const HTTP_OK_STATUS = 200;
 const PORT = process.env.PORT || '3001';
 
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.status(HTTP_OK_STATUS).send();
+});
+
+app.get('/talker', async (_req, res) => {
+  const data = JSON.parse(fs.readFileSync(json, 'utf8'));
+  res.status(200).json(data);
 });
 
 app.listen(PORT, () => {
